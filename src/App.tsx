7aad2c66127f47728b5ae6e59a55107b5dd43226
AlexanderTarler <saltdev@dev-salts-MBP>
2023-02-27 16:57:28 +0100

    Push the whole thing in on go

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { timezones } from './timezone-db';
 import './App.css';
+
+type TimeComponentProps = {
+  timezone: string;
+  location: string;
+};
+
+function Clock(props: TimeComponentProps) {
+  const [clocks, setClocks] = React.useState('');
+  useEffect(() => {
+    setInterval(() => {
+      let newDate = new Date();
+      let localTime = newDate.toLocaleString('en', {
+        timeZone: props.timezone,
+        hour12: false,
+      });
+      setClocks(localTime.toString());
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="clock-card">
+      <h1>{clocks.split(',')[1]}</h1>
+      <p>{props.location}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className="header">World Clocks</header>
+      <div className="board">
+        <Clock timezone={'Europe/Stockholm'} location={'Stockholm'} />
+        <Clock timezone={'Europe/London'} location={'London'} />
+        <Clock timezone={'Europe/Paris'} location={'Paris'} />
+        <Clock timezone={'Europe/Moscow'} location={'Moscow'} />
+        <Clock timezone={'Asia/Tokyo'} location={'Tokyo'} />
+        <Clock timezone={'America/New_York'} location={'New York'} />
+        <Clock timezone={'America/Los_Angeles'} location={'Los Angeles'} />
+        <Clock timezone={'America/Sao_Paulo'} location={'Sao Paulo'} />
+        <Clock timezone={'Australia/Brisbane'} location={'Brisbane'} />
+      </div>
     </div>
   );
 }
